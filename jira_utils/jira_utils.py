d95cfd7271f94b4dc70fda92ade90ae87d081267
ragban01 <raghavendra.bandimutt@arm.com>
2021-11-19 15:43:46 +0000

    Updating Jira Utils

--- jira_utils/jira_utils.py
+++ jira_utils/jira_utils.py
@@ -2,8 +2,169 @@ from jira import JIRA
 import pprint
 import time
 from appscript import app, k
+import jira
 import keyring
-import pprint
+from pprint import pprint
+import re
+
+def issue_to_dict(issue):
+    dict = {}
+    dict["key"]    = issue.key
+    dict["type"]   = issue.fields.issuetype.name
+    dict["status"] = issue.fields.status.name
+    try:
+        dict["priority"] = issue.fields.priority.name
+    except AttributeError:
+        pass
+    try:
+        dict["resolution"] = issue.fields.resolution.name
+    except AttributeError:
+        pass
+    try:
+        for version in issue.fields.fixVersions:
+            if "fix_version" not in dict.keys():
+                dict["fix_version"] = []
+            dict["fix_version"].append(version.name)
+    except AttributeError:
+        pass
+    try:
+       if issue.fields.versions:
+           for version in issue.fields.versions:
+               if "affect_version" not in dict.keys():
+                   dict["affect_version"] = []
+               dict["affect_version"].append(version.name)
+    except AttributeError:
+        pass
+       
+    try:
+        for component in issue.fields.components:
+            if "components" not in dict.keys():
+                dict["components"] = []
+            dict["components"].append(component.name)
+    except AttributeError:
+        pass
+
+    try:
+        for label in issue.fields.labels:
+            if "labels" not in dict.keys():
+                dict["labels"] = []
+            dict["labels"].append(label)
+    except AttributeError:
+        pass
+    try:
+        dict["revision_found"] = issue.fields.customfield_10287
+    except AttributeError:
+        pass
+    try:
+        for comment in issue.fields.comment.comments:
+            if "comments" not in dict.keys():
+                dict["comments"] = []
+            dict["comments"].append({"user_id"  : comment.author.name,
+                                    "email_id" : comment.author.emailAddress,
+                                    "body"     : comment.body})
+    except AttributeError:
+        pass
+
+    try:
+        dict["reason_introduced"] = issue.fields.customfield_26631.value
+    except:
+        pass
+
+    try:
+        dict["time_remaining"] = issue.fields.timetracking.remainingEstimate
+    except:
+        pass
+
+    dict["assignee"] = {"user_id"   : issue.fields.assignee.name,
+                        "full_name" : issue.fields.assignee.displayName,
+                        "email_id"  : issue.fields.assignee.emailAddress}
+    dict["reporter"] = {"user_id"   : issue.fields.reporter.name,
+                        "full_name" : issue.fields.reporter.displayName,
+                        "email_id"  : issue.fields.reporter.emailAddress}
+    
+    dict["summary"] = issue.fields.summary
+    return(dict)
+
+def get_all_open_actions(user_ids):
+    user_name = keyring.get_password("system","usr")
+    jira = JIRA(options={'server':"https://jira.arm.com/"},basic_auth=(user_name,keyring.get_password("system",user_name)))
+    assignee = "("
+    for user_id in user_ids:
+        index = user_ids.index(user_id) + 1
+        assignee += f"(assignee = {user_id})"
+        if index != len(user_ids):
+            assignee += " OR "
+    assignee += ")"
+    jira_issues = jira.search_issues(f"{assignee} AND Resolution = Unresolved AND Type = Action")
+    issues = []
+    for issue in jira_issues:
+        issues.append(issue_to_dict(issue))
+    return(issues)
+
+def get_all_open_bugs(user_ids):
+    user_name = keyring.get_password("system","usr")
+    jira = JIRA(options={'server':"https://jira.arm.com/"},basic_auth=(user_name,keyring.get_password("system",user_name)))
+    assignee = "("
+    for user_id in user_ids:
+        index = user_ids.index(user_id) + 1
+        assignee += f"(assignee = {user_id})"
+        if index != len(user_ids):
+            assignee += " OR "
+    assignee += ")"
+    jira_issues = jira.search_issues(f"{assignee} AND Resolution = Unresolved AND Type = Bug")
+    issues = []
+    for issue in jira_issues:
+        issues.append(issue_to_dict(issue))
+    return(issues)  
+
+def get_all_open_tasks(user_ids):
+    user_name = keyring.get_password("system","usr")
+    jira = JIRA(options={'server':"https://jira.arm.com/"},basic_auth=(user_name,keyring.get_password("system",user_name)))
+    assignee = "("
+    for user_id in user_ids:
+        index = user_ids.index(user_id) + 1
+        assignee += f"(assignee = {user_id})"
+        if index != len(user_ids):
+            assignee += " OR "
+    assignee += ")"
+    jira_issues = jira.search_issues(f"{assignee} AND Resolution = Unresolved AND Type = 'Scheduled Task'")
+    issues = []
+    for issue in jira_issues:
+        issue = jira.issue(issue.key)
+        issues.append(issue_to_dict(issue))
+    return(issues) 
+
+
+def create_jira_info_content(sheet,open_actions,actions_jira_ids):
+    mod_sheet = {}
+    mod_sheet['sheet_name'] = sheet['sheet_name']
+    mod_sheet['col_list'] = sheet ['col_list']
+    mod_sheet['content'] = []
+    for action in open_actions:
+
+        link_jira = f'=HYPERLINK("https://jira.arm.com/browse/{action["key"]}","{action["key"]}")'
+        content = {"JIRA ID"      : link_jira,
+                   "Summary"      : action['summary'],
+                   "Assignee"     : action['assignee']['user_id']}
+        fix_version_str = ""
+        if "fix_version" in action.keys():
+            for version in action['fix_version']:
+                index = action['fix_version'].index(version) + 1
+                fix_version_str += version
+                if index != len(action['fix_version']):
+                    fix_version_str += ","
+            content.update({"Fix Versions" : fix_version_str})
+    
+        if "time_remaining" in action.keys():
+            content.update({"time_remaining":action['time_remaining']})
+
+        if action['key'] in actions_jira_ids:
+            index = actions_jira_ids.index(action['key'])
+            content['Comments'] = sheet['content'][index]['Comments']
+        else:
+            content.update({"Comments" : ""})
+        mod_sheet['content'].append(content)
+    return(mod_sheet)
 
 class JIRAUSER():
 
@@ -19,8 +180,8 @@ class JIRAUSER():
         if "cc_email_id" in user_info.keys():
             for email_id in user_info["cc_email_id"]:
                 self.cc_email_id.append(email_id)
-
-        self.jira = JIRA(options={'server':"https://jira.arm.com/"},basic_auth=("ragban01",keyring.get_password("system","ragban01")))
+        user_name = keyring.get_password("system","usr")
+        self.jira = JIRA(options={'server':"https://jira.arm.com/"},basic_auth=(user_name,keyring.get_password("system",user_name)))
         self.issues = []
 
     ## Function gets all the open actions for a user 
@@ -61,8 +222,8 @@ class JIRAUSER():
         return(content)
 
     ## Functions which returns a HTML string which is list of all open Jiras without fixrevisions
-    def get_all_issues_without_fix_versions(self):
-        jira_issues = self.jira.search_issues(f"(assignee = {self.user_id} or reporter={self.user_id}) AND Resolution = Unresolved AND project = PJ02932")
+    def get_all_issues_without_fix_versions(self,project):
+        jira_issues = self.jira.search_issues(f"(assignee = {self.user_id} or reporter={self.user_id}) AND Resolution = Unresolved AND project = {project}")
         self.issues = []
         for issue in jira_issues:
             local_issue_dict = self.issue_to_dict(issue)
@@ -96,15 +257,41 @@ class JIRAUSER():
         return(content)
 
     # Function to get all task of reporter which is fixed but not closed
-    def get_all_fixed_tasked_not_closed(self):
-        jira_issues = self.jira.search_issues(f"reporter = {self.user_id} AND Resolution = Fixed AND status = Resolved ")
+    def get_all_fixed_not_closed(self,project):
+        jira_issues = self.jira.search_issues(f"reporter = {self.user_id} AND Resolution = Fixed AND status = Resolved AND project = {project}")
+        self.issues = []
+        for issue in jira_issues:
+            self.issues.append(self.issue_to_dict(issue))
         if len(self.issues) > 0:
-            content = "<h2><u> Summary of all tasks that are resolved but not closed </u></h2><br>"
+            content = "<h2><u> Summary of all Jiras that are resolved but not closed </u></h2><br>"
         else:
             content = ""
         for issue in self.issues:
             content += f'<u><a href="https://jira.arm.com/browse/{issue["key"]}"> {issue["key"]} </a> </u> : {issue["summary"]}<br>'
         return(content)    
+
+    # Function to get all task which is fixed but no reason introduced
+    def get_all_fixed_no_reason(self,project):
+        jira_issues = self.jira.search_issues(f"(reporter = {self.user_id} or assignee = {self.user_id}) AND Resolution = Fixed AND status = Resolved AND project = {project}")
+        self.issues = []
+        for issue in jira_issues:
+            issue_dict = self.issue_to_dict(issue)
+            if "reason_introduced" not in issue_dict.keys():
+                self.issues.append(self.issue_to_dict(issue))
+        if len(self.issues) > 0:
+            content = "<h2><u> Summary of all Jiras that are resolved but no reason introduced </u></h2><br>"
+        else:
+            content = ""
+        for issue in self.issues:
+            content += f'<u><a href="https://jira.arm.com/browse/{issue["key"]}"> {issue["key"]} </a> </u> : {issue["summary"]}<br>'
+        return(content)        
+        
+        
+    def get_all_issues(self,search_criteria):
+        jira_issues = self.jira.search_issues(search_criteria)
+        self.issues = []
+        for issue in jira_issues:
+            self.issues.append(self.issue_to_dict(issue))
 
     def issue_to_dict(self,issue):
         dict = {}
@@ -144,7 +331,7 @@ class JIRAUSER():
             for component in issue.fields.components:
                 if "components" not in dict.keys():
                     dict["components"] = []
-                dict["components"].append(component)
+                dict["components"].append(component.name)
         except AttributeError:
             pass
 
@@ -172,6 +359,10 @@ class JIRAUSER():
         except AttributeError:
             pass
 
+        try:
+            dict["reason_introduced"] = issue.fields.customfield_26631.value
+        except:
+            pass
 
         dict["assignee"] = {"user_id"   : issue.fields.assignee.name,
                             "full_name" : issue.fields.assignee.displayName,
@@ -209,6 +400,7 @@ class JIRAUSER():
         
 
         for email_id in self.cc_email_id:
+            print(f"cc {email_id}")
             msg.make(
                 new=k.cc_recipient,
                 with_properties={
